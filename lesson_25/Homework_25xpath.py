f613603d735b2644d45558c49c916373a6018dfc
Serhii Alokhin <serhiialokhinqa@gmail.com>
2024-10-09 09:55:15 +0300

    Виконані зміни HW#25

---
+++ lesson_25/Homework_25xpath.py
@@ -0,0 +1,52 @@
+# 1. Guest log in
+xpath1 = "//button[text()='Guest log in']'
+# 2. Sign in
+xpath2 = "//button[text()='Sign In']"
+# 3. Input Email
+xpath3 = '//*[@id="signinEmail"]'
+# 4. Input Password
+xpath4 = '//*[@id="signinPassword"]'
+# 5. Remember checkbox
+xpath5 = '//*[@id="remember"]'
+# 6. Restore password
+xpath6 = '//button[text()="Forgot password"]'
+# 7. Registration
+xpath7 = '//button[text()="Registration"]'
+# 8. Login button
+xpath8 = '//button[text()="Login"]'
+# 9. Registration Name
+xpath10 = '//*[@id="signupName"]'
+# 10. Registration Last name
+xpath11 = '//*[@id="signupLastName"]'
+# 11. Registration Email
+xpath12 = '//*[@id="signupEmail"]'
+# 12. Registration password
+xpath13 = '//*[@id="signupPassword"]'
+# 13. Registration re-password
+xpath14 = '//*[@id="signupRepeatPassword"]'
+# 14. Registration confirm button
+xpath15 = '//button[text()="Register"]'
+# 15. Exit from registration
+xpath16 = "//button[@aria-label='Close']"
+# 16. Sign up button
+xpath17 = '//button[text()="Sign up"]'
+# 17. YouTube Play
+xpath18 = "//button[contains(@class, 'ytp-large-play-button')]"
+# 18. YouTube redirect
+xpath19 = '//*[@id="movie_player"]/a[1]'
+# 19. Facebook
+xpath20 = "//a[contains(@href, 'https://www.facebook.com/Hillel.IT.School')]"
+# 20. Telegram
+xpath21 = "//a[contains(@href, 't.me')]"
+# 21. YouTube
+xpath22 = "//a[contains(@href, 'https://www.youtube.com/user/HillelITSchool?sub_confirmation=1')]"
+# 22. Instagram
+xpath23 = "//a[contains(@href, 'https://www.instagram.com/hillel_itschool/')]"
+# 23. LinkedIn
+xpath24 = "//a[contains(@href, 'https://www.linkedin.com/school/ithillel//')]"
+# 24. Hillel
+xpath25 = "//a[contains(@href, 'https://ithillel.ua/')]"
+# 25. Support
+xpath26 = "//a[contains(@href, 'mailto:developer@ithillel.ua')]"
+
+
